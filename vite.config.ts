1cfd103b932a6d8eaf27aa9ec601abe0cef2e7ef
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2026-04-15 17:14:43 +0200

    serve myPortp

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/',
+  base: '/myPorto/',
 })
